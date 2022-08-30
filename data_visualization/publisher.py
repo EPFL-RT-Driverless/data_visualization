eed415f7a165ec25cb1aafac5f057cc8396abe0d
Ted <oancea.tudor@icloud.com>
2022-08-30 12:18:39 +0200

    :construction: modified stuff in Publisher class, needs testing

--- data_visualization/publisher.py
+++ data_visualization/publisher.py
@@ -1,93 +1,114 @@
 #  Copyright (c) 2022. Tudor Oancea, Mattéo Berthet EPFL Racing Team Driverless
 import pickle
-import socket
+import signal
+from queue import Queue
+from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 from time import sleep
-import queue
-import signal
+
+from typing import Union
+from .constants import DEFAULT_HOST, DEFAULT_PORT
 
 __all__ = ["Publisher"]
 
 
-is_killed = False
-
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 1024  # The port used by the server
-
-
 class Publisher:
-    def __init__(self):
-        self.kill_thread = False
-        signal.signal(signal.SIGINT, self.signal_handler)
-        self.queue = queue.Queue()
-        self.state_history = []
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind((HOST, PORT))
-        self.s.listen(5)
-        self.thread = Thread(target=self.server_thread, args=(self.s,))
-        self.thread.start()
+    """
+    Class used to send data to a Subscriber class that is to be plotted in live dynamic mode.
+    It uses a socket connection that is dealt with in an auxiliary thread.
+    """
 
-    def update(self, data, cones, image):
-        # self.queue.put(np.array([data, cones, image], dtype=object))
-        # self.queue.put((data, cones, image))
-        self.queue.put([data, cones, 0])
-        print("updated", self.queue.qsize())
+    host: str  # Hostname of the machine on which runs the Subscriber.
+    port: int  # port to which to connect
+    _kill_thread: bool  # used to stop the auxiliary thread when the main thread is killed
+    _msg_queue: Queue  # queue used to send data to publish to the auxiliary thread
+    msg_history: list  # list of all the messages sent
+    _publisher_socket: socket  # socket used to connect to the Subscriber
+    _publisher_thread: Thread  # thread used to send data to the Subscriber
+    verbose: bool  # if True, print the status messages
 
-    def signal_handler(self, signal, frame):
+    def __init__(self, host: str = DEFAULT_HOST, port: int = DEFAULT_PORT, **kwargs):
+        self.host = host
+        self.port = port
+        self._kill_thread = False
+        self._msg_queue = Queue()
+        self.msg_history = []
 
-        print("You pressed Ctrl+C!")
+        self.verbose = False
+        for possible_kw in ["debug", "verbose"]:
+            if possible_kw in kwargs:
+                self.verbose = True
+                break
 
-        self.kill_thread = True
+        # connect SIGINT (signal sent when pressing ctrl+C or when killing the program) to stop the auxiliary thread and properly close the socket
+        signal.signal(signal.SIGINT, self._signal_handler)
 
-        print(self.state_history)
+        # connect the socket
+        self._publisher_socket = socket(AF_INET, SOCK_STREAM)
+        self._publisher_socket.bind((host, port))
+        self._publisher_socket.listen(5)
 
-        self.thread.join()
-        print("Server thread is joined")
+        # start the auxiliary thread
+        self._publisher_thread = Thread(
+            target=self._publisher_thread_target, args=(self._publisher_socket,)
+        )
+        self._publisher_thread.start()
 
-        self.s.close()
-        print("Socket is closed")
+    def _print_status_message(self, msg):
+        if self.verbose:
+            print("[PUBLISHER] : " + msg)
 
+    def update_queue(self, msg: Union[str, dict]):
+        self._msg_queue.put(msg)
+        self._print_status_message(
+            "updated queue, new size: {}".format(self._msg_queue.qsize())
+        )
+
+    def _signal_handler(self, si, frame):
+        self._print_status_message("You pressed Ctrl+C!")
+        self._kill_thread = True
+        self._print_status_message(self.msg_history)
+        self._publisher_thread.join()
+        self._print_status_message("Server thread is joined")
+        self._publisher_socket.close()
+        self._print_status_message("Socket is closed")
         sleep(1)
+        self._print_status_message("off")
 
-        print("Off")
+    # def get_all_queue_result(self):
+    #     result_list = []
+    #     while not self.queue.empty():
+    #         result_list.append(self.queue.get_nowait())
+    #
+    #     print(len(result_list))
+    #     return result_list
 
-    def get_all_queue_result(self):
-
-        result_list = []
-
-        while not self.queue.empty():
-            result_list.append(self.queue.get_nowait())
-
-        print(len(result_list))
-
-        return result_list
-
-    def server_thread(self, s):
-        print("Server is ready ...")
+    def _publisher_thread_target(self, s):
+        self._print_status_message("Server is ready ...")
 
         s.settimeout(15)
         client, adr = s.accept()
 
-        print("Connection established")
+        self._print_status_message("Connection established")
         while True:
-            if self.kill_thread:
-                print("Switching off...")
+            if self._kill_thread:
+                self._print_status_message("Switching off...")
                 break
-            # print(f'Connection to {adr} established')
+                # self._print_status_message()(f'Connection to {adr} established')
 
-            if self.queue.empty():
-                # print("Queue is empty")
+            if self._msg_queue.empty():
+                # self._print_status_message()("Queue is empty")
                 # client.send(pickle.dumps("empty"))
                 sleep(0.1)
             else:
                 # client.send(pickle.dumps(self.get_all_queue_result()))
-                data = pickle.dumps(self.queue.get_nowait())
-                size = len(data)
-                # print(data)
-                # print("presend")
+                data = pickle.dumps(self._msg_queue.get_nowait())
+                # size = len(data)
+                # self._print_status_message()(data)
+                # self._print_status_message()("presend")
                 # self.s.sendall(struct.pack(">L", size) + data) #'''struct.pack(">L") + '''
                 client.sendall(data)
-                print("sent")
+                self._print_status_message("sent")
 
         client.close()
-        print("Connection closed")
+        self._print_status_message("Connection closed")
